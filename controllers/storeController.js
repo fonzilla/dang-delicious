971da29332814753610e2f02c5ee7195ca673b21
fonzilla <j.alfonso.antonio@gmail.com>
2017-05-15 00:22:37 -0400

    added geolocation and photo upload

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next){
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    }else {
+      next({ message: 'That filetype is not allowed!' }, false)
+    }
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -8,6 +23,25 @@ exports.homePage = (req, res) => {
 exports.addStore = (req,res) => {
   res.render('editStore', { title: 'Add Store'});
 };
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  //check if there is no new file to resize
+  if(!req.file){
+    next(); //skip to next middleware
+    return;
+  }
+  console.log(req.file);
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  //now resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+}
 
 exports.createStore = async (req,res) => {
   const store = await (new Store(req.body)).save();
@@ -27,10 +61,13 @@ exports.editStore = async (req,res) => {
 };
 
 exports.updateStore = async (req,res) => {
+  req.body.location.type = 'Point'
+  //find and update store'
   const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
     new: true,
     runValidators: true
   }).exec();
   req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
   res.redirect(`/stores/${store.id}/edit`);
+  //redirect to the store and tell user it worked
 };
